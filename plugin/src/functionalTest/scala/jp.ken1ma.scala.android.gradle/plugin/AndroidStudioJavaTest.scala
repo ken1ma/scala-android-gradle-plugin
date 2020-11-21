377b628e9d6948765f7c470860149f214fa38eb0
Kenichi Masuko <kenichi@sysart.jp>
2020-11-21 12:16:36 +0900

    Test Android Studio 4 Java Project

---
+++ plugin/src/functionalTest/scala/jp.ken1ma.scala.android.gradle/plugin/AndroidStudioJavaTest.scala
@@ -0,0 +1,49 @@
+package jp.ken1ma.scala.android.gradle
+package plugin
+
+import java.nio.file.{Paths, Files}
+import org.gradle.testkit.runner.GradleRunner
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatestplus.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class AndroidStudioJavaTest extends AnyFreeSpec {
+  // Make sure the Android Gradle plugin works in the environment and the connected Android device without the plugin
+  "Plain Android Java project" in {
+    val projectDir = Paths.get("src/functionalTest/projects/AndroidStudioJava")
+    GradleAndroidTestHelper.generateLocalProperties(projectDir)
+
+    val runner = GradleRunner.create
+    runner.withProjectDir(projectDir.toFile)
+    runner.withPluginClasspath
+    runner.forwardOutput
+
+    assert(runner.withArguments("uninstallAll").build.getOutput.contains("BUILD SUCCESSFUL"))
+    assert(runner.withArguments("clean").build.getOutput.contains("BUILD SUCCESSFUL"))
+
+    // build APKs (depends on assembleDebug and assembleRelease)
+    val buildOutput = runner.withArguments("build").build.getOutput
+    assert(buildOutput.contains("BUILD SUCCESSFUL"))
+
+    // Run unit tests (depends on testDebugUnitTest and testReleaseUnitTest)
+    val testOutput = runner.withArguments("test").build.getOutput
+    assert(testOutput.contains("BUILD SUCCESSFUL")) // "BUILD FAILED" instead when a test fails
+
+    // Run instrumented tests (on a device)
+    // this can fail with "com.android.builder.testing.api.DeviceException: No connected devices!"
+    // TODO: should installDebugAndroidTest be tested too? (connectedDebugAndroidTest doesn't include installDebugAndroidTest)
+    val androidTestOutput = runner.withArguments("connectedDebugAndroidTest").build.getOutput
+    assert(androidTestOutput.contains("BUILD SUCCESSFUL")) // "BUILD FAILED" instead when a test fails
+
+    assert(runner.withArguments("uninstallAll").build.getOutput.contains("BUILD SUCCESSFUL"))
+
+    // make sure the tasks to replace still exist in the Android Gradle plugin version
+    assert(buildOutput      .contains("Task :app:compileDebugJavaWithJavac"))
+    assert(buildOutput      .contains("Task :app:compileReleaseJavaWithJavac"))
+    assert(testOutput       .contains("Task :app:compileDebugUnitTestJavaWithJavac"))
+    assert(testOutput       .contains("Task :app:compileReleaseUnitTestJavaWithJavac"))
+    assert(androidTestOutput.contains("Task :app:compileDebugAndroidTestJavaWithJavac"))
+  }
+}
--- plugin/src/test/scala/jp.ken1ma.scala.android.gradle/GradleAndroidTestHelper.scala
+++ plugin/src/test/scala/jp.ken1ma.scala.android.gradle/GradleAndroidTestHelper.scala
@@ -5,9 +5,16 @@ import java.io.File
 
 object GradleAndroidTestHelper {
   def generateLocalProperties(projectDir: Path): Unit = {
-    Files.writeString(projectDir.resolve("local.properties"), s"""
-      sdk.dir=${AndroidTestHelper.ANDROID_SDK_ROOT}
-    """)
+    if (!Files.exists(projectDir))
+      throw new IllegalArgumentException(s"Path not found: ${projectDir.toAbsolutePath}")
+
+    else if (!Files.isDirectory(projectDir))
+      throw new IllegalArgumentException(s"Path is not a directory: ${projectDir.toAbsolutePath}")
+
+    else
+      Files.writeString(projectDir.resolve("local.properties"), s"""
+        sdk.dir=${AndroidTestHelper.ANDROID_SDK_ROOT}
+      """)
   }
 
   def generateLocalProperties(projectDir: File): Unit =
