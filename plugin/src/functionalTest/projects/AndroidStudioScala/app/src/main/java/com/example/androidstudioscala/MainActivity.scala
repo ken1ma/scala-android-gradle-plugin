fb8908a1f7beeec51e9e91342a3f2d0ce699a2fd
Kenichi Masuko <kenichi@sysart.jp>
2020-11-22 12:23:07 +0900

    Display the version instead of Hello World

--- plugin/src/functionalTest/projects/AndroidStudioScala/app/src/main/java/com/example/androidstudioscala/MainActivity.scala
+++ plugin/src/functionalTest/projects/AndroidStudioScala/app/src/main/java/com/example/androidstudioscala/MainActivity.scala
@@ -7,5 +7,8 @@ class MainActivity extends AppCompatActivity {
   override def onCreate(savedInstanceState: Bundle) = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    import BuildConfig.{VERSION_NAME, VERSION_CODE}
+    findViewById(R.id.textView0).asInstanceOf[android.widget.TextView].setText(s"version = $VERSION_NAME ($VERSION_CODE)")
   }
 }
